4aec6dc76949db387006954cfc724f8a5350745c
fafasonga <s.fabrice@innopolis.ru>
2018-02-09 19:52:42 +0300

    Lab 03

--- Lab 04/new.py
+++
@@ -1,8 +0,0 @@
-
-B = int(input("Enter The Number of Plain Text to Encrypt: "))
-
-arr = []
-for i in range(B):
-    M = (input("Enter the values of Plain Text: "))
-    arr.extend(M)
-    print(arr)
